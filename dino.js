9ec1357b7f3cf4a34f07c8966a9c4d4df076b487
Shashoto Nur <nurulaziz.nz@gmail.com>
2021-08-13 17:19:01 +0600

    initial commit

---
+++ dino.js
@@ -0,0 +1,15 @@
+
+// Browse to chrome://dino
+// Open the web console
+// Paste any code whichever you like and press Enter
+// NOTE: Apply the two codes below sequentially
+
+// 1. Make Dino Immortal
+[Runner.prototype.gameOver, Runner.prototype.oldGameOver] = [() => {}, Runner.prototype.gameOver];
+
+// 2. Make Dino Mortal Again
+Runner.prototype.gameOver = Runner.prototype.oldGameOver;
+
+// 3. Get the Highest possible score
+Runner.prototype.distanceRan = (99999 * 30);
+Runner.prototype.gameOver();
